f518599a6ea444ff72402d0d8cd80382a1c546d6
Marwane <bahraoui.marwane@gmail.com>
2021-11-02 20:00:44 +0100

    ajout noeud + ajout arete v1

--- graph.cpp
+++ graph.cpp
@@ -5,7 +5,13 @@ int main()
 {
     graph mama = graph();
     mama.display_graph(); // graphe vide
-    mama.get_adjacencyList()[499]=5;
+    mama.add_vertice(1);
+    mama.add_vertice(2);
+    printf("%d",mama.add_edge(1,2));
+    printf("%d",mama.add_edge(1,2));
+    printf("%d",mama.add_edge(1,2));
+    printf("%d",mama.add_edge(2,1));
+    printf("%d",mama.add_edge(1,2));
     mama.display_graph(); // graphe pas vide (meme si auncun sens pr l'instant)
     printf("MAMA : %d\n",mama.get_adjacencyList()[5]);
     
--- graph.h
+++ graph.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_VERTICES 500
+#define MAX_VERTICES 5
 /**
  * @class graph
  * Cette classe permet de manipuler des graphes selon 
@@ -31,6 +31,21 @@ public:
         return adjacencyList;
     }
 
+
+    short int* actual_vertices(){
+        int cmpt=0,i;
+        short int* existingVertices = (short int*)malloc(MAX_VERTICES * sizeof(short int));
+        for (i = 0; i < MAX_VERTICES * (MAX_VERTICES-1); i+=(MAX_VERTICES-1))
+        {
+            if (adjacencyList[i] != -2)
+                existingVertices[cmpt]=1; // en c,  <=> true
+            else
+                existingVertices[cmpt]=0; // en c, 0 <=> false
+            cmpt++;
+        }
+        return existingVertices;
+    }
+
     /**
      * Affiche le graphe 
     */
@@ -49,23 +64,44 @@ public:
                         printf("%d  ",adjacencyList[(i * (MAX_VERTICES - 1)) + j]);
                 // printf("  ///i=%d,j=%d///\n",i,j); // ligne a utiliser pour debug cette methode
                 printf("\n");
+
             }
         }
         printf("\n##################################\n");
     }
 
-    short int* actual_vertices(){
-        int cmpt=0,i;
-        short int* existingVertices = (short int*)malloc(MAX_VERTICES * sizeof(short int));
-        for (i = 0; i < MAX_VERTICES * (MAX_VERTICES-1); i+=(MAX_VERTICES-1))
-        {
-            if (adjacencyList[i] != -2)
-                existingVertices[cmpt]=1; // en c,  <=> true
-            else
-                existingVertices[cmpt]=0; // en c, 0 <=> false
-            cmpt++;
-        }
-        return existingVertices;
+    bool add_vertice(short int verticeToAdd){
+        short int i;
+        if (adjacencyList[verticeToAdd*(MAX_VERTICES-1)] != -2 || verticeToAdd < 0 || verticeToAdd >= MAX_VERTICES)
+            // print (sommet existe deja) : a mettre la ou il sera utilise plus tard
+            return false;
+        for (i = verticeToAdd*(MAX_VERTICES-1); i < (verticeToAdd+1)*(MAX_VERTICES-1); i++)
+            adjacencyList[i]=-1;
+        return true;
     }
-   
+
+    bool add_edge(short int verticeSrc, short int verticeDest){
+        short int i;
+        if (adjacencyList[verticeSrc*(MAX_VERTICES-1)] == -2 || adjacencyList[verticeDest*(MAX_VERTICES-1)] == -2 || verticeDest==verticeSrc)
+            return false; // le cas ou au moins un des deux sommets n'existe pas
+        for (i = 0; i < MAX_VERTICES-1; i++)
+            if (adjacencyList[(verticeSrc * (MAX_VERTICES-1)) + i] == verticeDest)
+                return true; // false ou true ? // le cas ou il existe deja une arrete entre les deux sommets
+        for (i = 0; i < MAX_VERTICES-1; i++)
+        {
+            if (adjacencyList[(verticeSrc * (MAX_VERTICES-1)) + i] == -1) // recherche d'une place "libre" pour inserer le voisiin
+            {
+                adjacencyList[(verticeSrc * (MAX_VERTICES-1)) + i] = verticeDest;
+                for (i = 0; i < MAX_VERTICES-1; i++)
+                {
+                    if (adjacencyList[(verticeDest * (MAX_VERTICES-1)) + i] == -1) // idem
+                    {
+                        adjacencyList[(verticeDest * (MAX_VERTICES-1)) + i] = verticeSrc;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 };
